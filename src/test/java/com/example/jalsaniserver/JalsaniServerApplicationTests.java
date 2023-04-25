1dccc03b0ffa242ab8fdc60dd6de7f7d815c17e0
Kang JooYoung <joo.zero.10@gmail.com>
2023-04-25 11:28:13 +0900

    최초 세팅

---
+++ src/test/java/com/example/jalsaniserver/JalsaniServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jalsaniserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JalsaniServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
